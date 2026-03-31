c7f8b3cd4f3375bb35dd12f077c4c80cab3907dc
mmattbtw <matt@mmatt.net>
2026-03-30 23:57:10 -0500

    did

---
+++ src/pages/.well-known/did.json.ts
@@ -0,0 +1,34 @@
+export function GET() {
+  return new Response(
+    JSON.stringify({
+      "@context": [
+        "https://www.w3.org/ns/did/v1",
+        "https://w3id.org/security/multikey/v1",
+        "https://w3id.org/security/suites/secp256k1-2019/v1",
+      ],
+      id: "did:web:nashproto.com",
+      alsoKnownAs: ["at://nashproto.com"],
+      verificationMethod: [
+        {
+          id: "did:web:nashproto.com#atproto",
+          type: "Multikey",
+          controller: "did:web:nashproto.com",
+          publicKeyMultibase:
+            "zQ3shV9a5gKTkraUDNvjw2VnjRkmmYur1NiU8rFGVPhzy1XvS",
+        },
+      ],
+      service: [
+        {
+          id: "#atproto_pds",
+          type: "AtprotoPersonalDataServer",
+          serviceEndpoint: "https://teal.town",
+        },
+      ],
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+}
